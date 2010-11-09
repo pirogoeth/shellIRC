3ff40abaa00d05681af44024ad1aa1f21486a514
miyoko <miyoko@maio.me>
2010-11-09 16:51:01 -0600

    fixed issues with commit_info stuff in modules/github.sh

--- modules/github.sh
+++ modules/github.sh
@@ -3,28 +3,29 @@
 
 github_repo_info () {
 	checkfor_branch () {
-		if [ -z $2 ] ; then
+		if [ -z $1 ] ; then
 			branch="master"
 		else
-			branch=$2
+			branch=$1
 		fi
 	}
 	string=$(echo $1 | sed -e 's/\// /;s/://')
 	user=$(echo $string | awk '{print $1}')
 	repo=$(echo $string | awk '{print $2}')
-	base="http://github.com/api/v2/yaml/"
+	base="http://github.com/api/v2/yaml"
+	checkfor_branch $2
 	repo_info="repos/show/$user/$repo"
-	commits_info="commits/list/$user/$repo/$branch"
-	repo_api_ret=$(curl -s $base$repo_info)
-	#commits_api_ret=$(curl -s $base$commits_info)
+	commit_info="commits/list/$user/$repo/$branch"
+	repo_api_ret=$(curl -s $base/$repo_info)
+	commit_api_ret=$(curl -s $base/$commit_info)
 	repo_name=$(echo "$repo_api_ret" | grep -Eo ":name:(.*)" | sed -e 's/:.*://')
 	repo_owner=$(echo "$repo_api_ret" | grep -Eo ":owner:(.*)" | sed -e 's/:.*://')
 	repo_description=$(echo "$repo_api_ret" | grep -Eo ":description:(.*)" | sed -e 's/:.*://')
 	repo_url=$(echo "$repo_api_ret" | grep -Eo ":url:(.*)" | sed -e 's/:.*://')
-	#commit_url=$(echo "$commits_api_ret" | grep -Eo "url:" | sed -e 's/.*://')
-	#commit_sender=$(echo "$commits_api_ret" | grep -Eo -A1 "committer:" | grep -Eo "name:.*" | sed -e 's/.*://g')
+	commit_url=$(echo "$commit_api_ret" | grep -Eo -C1 -m1 "  url:(.*)" | sed -e 's/  url: //')
+	commit_sender=$(echo "$commit_api_ret" | grep -E -A1 -m1 "committer:(.*)" | sed -e 's/.*://g' | tr -d [:space:])
 	struct_repo="Repo Name: $repo_name || Repo Owner: $repo_owner || Repo Description: $repo_description || Repo URL: http:$repo_url"
-	#struct_commit="Latest commit by: $commit_sender || http://github.com/$commit_url"
+	struct_commit="Latest commit by: $commit_sender || http://github.com/$commit_url"
 	msg $dest $struct_repo
-	#msg $dest $struct_commit
+	msg $dest $struct_commit
 }
