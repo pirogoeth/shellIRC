300bcf6e3062fd63be607ab049f8f329b252180e
miyoko <miyoko@maio.me>
2010-12-12 10:56:11 -0600

    initial commit with a few modules

---
+++ core/oper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# oper.sh -- hook in to shellbot for opering up the bot
+
+if [ "$(echo $cmd | cut -b 1-5)" == "^oper" ] ; then
+	. etc/mod_ope. etc/mod_oper.sh
+	echo "OPER $oper_username $oper_password" >> etc/core_input
+fi
---
+++ core/ping.sh
@@ -0,0 +1,13 @@
+# ping module
+
+if [ "$(echo "$cmd" | cut -b 1-5)" == "^ping" ] ; then
+	send_raw PING $send_nick
+	ping_nick=$send_nick
+	ping_send=$(date +%s.%N)
+	ping_recv=$recv_chan
+fi
+
+if [ $command == "PONG" ] ; then
+	ping_time=$(echo "$(date +%s.%N) - $ping_send" | bc)
+	msg $ping_recv ping time for $ping_nick: $ping_time seconds
+fi
