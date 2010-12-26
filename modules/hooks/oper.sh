025d87b3d1946233b7e31d9f4b080d00b0186f5a
root <root@crystalball.(none)>
2010-12-26 03:33:33 +0100

    lots of changes, mainly changed getopts params, multiserver support, fixed nickserv identification, added command prefix specification, nickserv identification method changed,changed some socket stuff, rearranged a few things.

--- modules/hooks/oper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# oper.sh -- hook in to shellbot for opering up the bot
-
-if [ "$(echo $cmd | cut -b 1-5)" == "^oper" ] ; then
-	. etc/mod_oper.sh
-	echo "OPER $oper_username $oper_password" >> etc/core_input
-fi
