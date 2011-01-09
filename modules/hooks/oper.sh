a4b16589618b8d8ab4fba53cc9a8e4223b73d553
miyoko <miyoko@maio.me>
2011-01-09 12:53:56 -0600

    structure fixes

--- modules/hooks/oper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# oper.sh -- hook in to shellbot for opering up the bot
-
-if [ "$(echo $cmd | cut -b 1-5)" == $prefix"oper" ] ; then
-	. etc/mod_oper.sh
-	echo "OPER $oper_username $oper_password" >> etc/core_input
-fi
