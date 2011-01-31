f6526ff0f421e0dabca0401aa7232f823ea2981b
miyoko <miyoko@maio.me>
2011-01-31 07:26:57 -0600

    added wolfram search modules

---
+++ core/wolfram/wolfram.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from urllib import urlencode, urlopen
+from xml.etree.cElementTree import fromstring
+import sys
+
+appId = '395X7T-8JLXGEP8YH'
+
+def search(query, format=('plaintext', )):
+    return parseWolframAlphaResponse(urlopen('http://api.wolframalpha.com/v2/query?%s' % (
+            urlencode(
+                {
+                    'appid': appId,
+                    'input': query,
+                    'format': ','.join(format)
+                }
+            )
+        )
+    ).read())
+def parseWolframAlphaResponse(response, redirected = False):
+    xmlTree = fromstring(response)
+    recalculate = xmlTree.get('recalculate')
+    if recalculate:
+        if not redirected:
+            return parseWolframAlphaResponse(urllib.urlopen(recalculate).read(), True)
+        else:
+            return 'Error', 'Too many redirects.'
+    else:
+        success = xmlTree.get('success')
+        if success == 'true':
+            out = []
+            for pod in xmlTree.findall('pod'):
+                title = pod.get('title')
+                plaintext = pod.find('subpod/plaintext')
+                if plaintext is not None and plaintext.text:
+                    out.append((title, plaintext.text.split('\n')))
+                    
+            return True, out
+        else:
+            return False, [tip.get('text') for tip in xmlTree.findall('tips/tip')]
+
+if __name__ == "__main__":
+    try:
+        text = sys.stdin.readlines()
+        result = search(" ".join(text))
+        bool, response = result
+        for title, poddle in response:
+	    print ' \x02', title, '\n', " ".join(poddle), '\n'
+    except (IndexError, TypeError):
+        print "lolwut."
---
+++ core/wolfram/wolfram.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $(echo $cmd | cut -b 1-3) == $prefix"wa" ] ; then
+	if test -e tmp/wrap; then msg $send_nick 'Query already in progress, try again in a few seconds.'; else
+		query=${text#* }
+		touch tmp/wrap
+		if test -e tmp/wra ; then rm tmp/wra; touch tmp/wra; fi
+		echo "$query" | ./include/wolfram.py 2>&1 1>tmp/wra
+		while read response
+			do
+				msg ${dest} "${response}"
+			done < tmp/wra
+		rm tmp/wrap
+	fi
+fi
